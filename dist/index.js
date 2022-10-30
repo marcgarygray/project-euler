8664feb2a6f82aa5bed5c6daba1122e3212941f8
Marc Gray <marcgarygray@gmail.com>
2022-10-29 21:17:53 -0700

    no dist (#8)
    Co-authored-by: marcgray <marc@marcgarygray.com>

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const _1_1 = require("./solutions/1");
-console.log((0, _1_1.sumOfMultiples)({ below: 10, divisors: [3, 5] })); // output: 23
-console.log((0, _1_1.sumOfMultiples)({ below: 1000, divisors: [3, 5] })); // output: 233168
